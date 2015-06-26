a18822297209301a5abe820e855dfd51a50e70f2
Zhennning.Su <329595547@qq.com>
2015-06-26 14:36:33 +0800

    with data

--- Untitled.m
+++ Untitled.m
@@ -1,8 +1,11 @@
-% detail = selfCreateData('603555','20150301','20150601');
-% detail(:,1) = datenum(int2str(int64(detail(:,1)*100)),'yyyymmddHHMMSS');
-% plot(detail(:,2))
-% plot(detail(:,1),detail(:,2))
-% detail(:,1) = floor(detail(:,1));
+clc;close all;clear
+
+detail = selfCreateData('600022','20150301','20150626');
+detail(:,1) = datenum(int2str(int64(detail(:,1)*100)),'yyyymmddHHMMSS');%按时间顺序排序
+figure(1)
+plot(detail(:,2))%绘制股价连续变化
+%plot(detail(:,1),detail(:,2))%绘制股价随时间变化
+detail(:,1) = floor(detail(:,1));
 y=zeros(size(detail,1),1);
 valuemin=[];
 valuemax=[];
@@ -21,3 +24,7 @@ for i = detail(1,1):detail(end,1)
     t=t+1;
 end
 % plot(y)
+figure(2)
+plot(valuemax);
+hold on
+plot(valuemin);
